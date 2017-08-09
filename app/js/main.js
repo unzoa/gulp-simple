66b0f1a45ece5e201e726cc523ff57ca949e74b4
unzoa <unzoa@icloud.com>
2017-08-09 09:21:06 +0800

    Update main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 var menuli = document.querySelector('.menu-con ul').getElementsByTagName('li'),
 	scroll_con = document.querySelector('.scroll-con').getElementsByTagName('div');
+var sonMoveMark = 0;
 
 for(var i= 0; i<menuli.length;i++){
 	menuli[i].onclick= function(){
 		let i = this.innerHTML.substring(this.innerHTML.length-1,this.innerHTML.length);
+		sonMoveMark = i-1;
 		for(var j=0;j<scroll_con.length;j++){
 			scroll_con[j].setAttribute('style','transform:translate3d(-'+(i-1)+'00%,0,0);transition:transform .5s ease');
 		}
@@ -11,7 +13,6 @@ for(var i= 0; i<menuli.length;i++){
 }
 
 // 自动执行
-var sonMoveMark = 0;
 setInterval(()=>{
 	if (sonMoveMark<scroll_con.length) {
 		for(var j=0;j<scroll_con.length;j++){
